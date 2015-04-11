f567a9d01bc135813e551922e735cc3e5632c2d5
Kat Betz <katbetz@ufl.edu>
2015-04-11 18:09:36 -0400

    added wallpaper

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+// Adds wallpaper plugin to the header and footer
+$('.header').wallpaper({
+	source: {
+		"fallback": "img/header.png",
+		"(min-width: 1000px)": "img/header.png"
+	}
+
+});
+
+
+});
